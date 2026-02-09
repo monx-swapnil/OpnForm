57b963e78fe67347d6a748b2451822321d75f52d
monx-swapnil <swapnil.srivastava@monx.co.in>
2026-02-09 20:29:57 +0530

    change root compatible to railway

--- client/middleware/root-redirect.js
+++ client/middleware/root-redirect.js
@@ -1,9 +1,6 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(() => {
   const route = useRoute()
-
-  // Only apply subdomain redirect AFTER login
-  // Never auto-redirect on root entry
-  if (route.path !== '/') return
-
-  return navigateTo('/login')
+  if (route.path === '/') {
+    return navigateTo('/login')
+  }
 })
