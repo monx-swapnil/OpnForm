2ab7bc6d7e04585cbc40e13d4b79f445fdb864ac
monx-swapnil <swapnil.srivastava@monx.co.in>
2026-02-10 19:11:02 +0530

    disable mail

--- api/app/Listeners/Forms/FormCreationConfirmation.php
+++ api/app/Listeners/Forms/FormCreationConfirmation.php
@@ -7,16 +7,15 @@ use App\Mail\Forms\FormCreationConfirmationMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 
-class FormCreationConfirmation implements ShouldQueue
+class FormCreationConfirmation
 {
-    /**
-     * Handle the event.
-     *
-     * @param  object  $event
-     * @return void
-     */
     public function handle(FormCreated $event)
     {
-        Mail::to($event->form->creator)->send(new FormCreationConfirmationMail($event->form));
+        if (config('app.self_hosted')) {
+            return;
+        }
+
+        Mail::to($event->form->creator)
+            ->send(new FormCreationConfirmationMail($event->form));
     }
 }
